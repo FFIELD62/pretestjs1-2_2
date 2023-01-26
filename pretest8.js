7dd31f5b2776148e0a99bf7a0184b76f1018ffd8
FFIELD62 <120073007+FFIELD62@users.noreply.github.com>
2023-01-26 10:55:12 +0700

    Update pretest8.js

--- pretest8.js
+++ pretest8.js
@@ -1,24 +1,21 @@
 function simplePair(num1,num){
     const str = [];
-    const str1 = 0; 
-
     for (let i = 0; i < num1.length; i++){
         
         for (let j = i+1; j < num1.length; j++){
             str[i] = num1[i] * num1[j];
-            srt1 = str[i];
+
             if (str[i] === num){
                 console.log (num," = ",num1[i]," + ",num1[j] );
             }
-            else if (i > num1.length && str[i] != num){
-                console.log("null");
-            }
+           
             
         }
             
         
     }
-    
+
+  
      
     
     
