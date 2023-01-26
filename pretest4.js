9f451bff0bfbd88cd49221a6d64e1db4425e44e8
FFIELD62 <120073007+FFIELD62@users.noreply.github.com>
2023-01-26 10:44:06 +0700

    Create pretest4.js

---
+++ pretest4.js
@@ -0,0 +1,9 @@
+function totalVolume(num){
+    const str1 = 0;
+    for (let str of num) {
+        str1 = str1 * str;
+        console.log(str1);
+    }
+
+}
+totalVolume ([4,2,4],[3,3,3],[1,1,2],[2,1,1])
